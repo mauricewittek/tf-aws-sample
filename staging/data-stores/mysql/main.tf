178e4db51a18458c95e96d3c9b60f30ba29ef4da
Maurice Wittek <maurice.wittek@gmail.com>
2022-06-22 08:19:08 +0200

    adds mysql db for webserver cluster

---
+++ staging/data-stores/mysql/main.tf
@@ -0,0 +1,26 @@
+terraform {
+  backend "s3" {
+    bucket = "tf-aws-sample-state"
+    key    = "staging/data-stores/mysql/terraform.tfstate"
+    region = "us-east-2"
+
+    dynamodb_table = "tf-aws-sample-locks"
+    encrypt        = true
+  }
+}
+
+provider "aws" {
+  region = "us-east-2"
+}
+
+resource "aws_db_instance" "example" {
+  identifier_prefix   = "tf-aws-sample"
+  engine              = "mysql"
+  allocated_storage   = 10
+  instance_class      = "db.t2.micro"
+  skip_final_snapshot = true
+  db_name             = "example_database"
+
+  username = var.db_username
+  password = var.db_password
+}
